5f355721a97a4d5370b12555c6ff81d93deedfc4
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-09-22 22:23:57 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import logger from "morgan";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import createError from "http-errors";
+
+import indexRouter from "./routes/index.js";
+
+const app = express();
+// eslint-disable-next-line no-underscore-dangle
+const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "pug");
+app.set("views", `${__dirname}/views`);
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+export default app;
