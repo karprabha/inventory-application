5acb9410bda02cb8b02d870fcc9933580e06d041
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-09-23 20:26:05 +0530

    add category display

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,13 +1,38 @@
 /* eslint-disable camelcase */
 import expressAsyncHandler from "express-async-handler";
 import Category from "../models/category.js";
+import Item from "../models/item.js";
 
 export const category_list = expressAsyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category list");
+    const allCategories = await Category.find({}, "name")
+        .sort({ name: 1 })
+        .exec();
+
+    res.render("category_list", {
+        title: "Category List",
+        category_list: allCategories,
+    });
 });
 
+// eslint-disable-next-line consistent-return
 export const category_detail = expressAsyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Category Detail: ${req.params.id}`);
+    const [category, itemsInCategory] = await Promise.all([
+        Category.findById(req.params.id).exec(),
+        Item.find({ category: req.params.id }, "name description").exec(),
+    ]);
+
+    if (category === null) {
+        const err = new Error("Category not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("category_detail", {
+        title: category.name,
+        // eslint-disable-next-line object-shorthand
+        category: category,
+        category_items: itemsInCategory,
+    });
 });
 
 export const category_create_get = expressAsyncHandler(
